0712453610672ae5edd59fdd86576103b7af978c
youn2029 <cocoa8090@gmail.com>
2019-04-16 15:48:49 +0900

    메인 스토리 보드 설정, 컨트롤러 연결

---
+++ Custom-SideBarDIY/FrontViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FrontViewController.swift
+//  Custom-SideBarDIY
+//
+//  Created by 윤성호 on 16/04/2019.
+//  Copyright © 2019 윤성호. All rights reserved.
+//
+
+import UIKit
+
+class FrontViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
